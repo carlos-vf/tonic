182805ad9a4b0dd4eaf9e0e1991e47cfe4e12f9d
carlos-vf <carlosvelazquezf@hotmail.com>
2025-05-29 20:20:12 +0200

    Reduced buffer size 1e6 -> 1e5. Modified keras' weights file path

--- tonic/train.py
+++ tonic/train.py
@@ -1,159 +1,149 @@
-'''Script used to train agents.'''
-
-import argparse
 import os
+import time
 
-import tonic
-import yaml
+import numpy as np
 
-
-def train(
-    header, agent, environment, test_environment, trainer, before_training,
-    after_training, parallel, sequential, seed, name, environment_name,
-    checkpoint, path
-):
-    '''Trains an agent on an environment.'''
-
-    # Capture the arguments to save them, e.g. to play with the trained agent.
-    args = dict(locals())
-
-    checkpoint_path = None
-
-    # Process the checkpoint path same way as in tonic.play
-    if path:
-        tonic.logger.log(f'Loading experiment from {path}')
-
-        # Use no checkpoint, the agent is freshly created.
-        if checkpoint == 'none' or agent is not None:
-            tonic.logger.log('Not loading any weights')
-
-        else:
-            checkpoint_path = os.path.join(path, 'checkpoints')
-            if not os.path.isdir(checkpoint_path):
-                tonic.logger.error(f'{checkpoint_path} is not a directory')
-                checkpoint_path = None
-
-            # List all the checkpoints.
-            checkpoint_ids = []
-            for file in os.listdir(checkpoint_path):
-                if file[:5] == 'step_':
-                    checkpoint_id = file.split('.')[0]
-                    checkpoint_ids.append(int(checkpoint_id[5:]))
-
-            if checkpoint_ids:
-                # Use the last checkpoint.
-                if checkpoint == 'last':
-                    checkpoint_id = max(checkpoint_ids)
-                    checkpoint_path = os.path.join(
-                        checkpoint_path, f'step_{checkpoint_id}')
-
-                # Use the specified checkpoint.
-                else:
-                    checkpoint_id = int(checkpoint)
-                    if checkpoint_id in checkpoint_ids:
-                        checkpoint_path = os.path.join(
-                            checkpoint_path, f'step_{checkpoint_id}')
-                    else:
-                        tonic.logger.error(f'Checkpoint {checkpoint_id} '
-                                           f'not found in {checkpoint_path}')
-                        checkpoint_path = None
-
-            else:
-                tonic.logger.error(f'No checkpoint found in {checkpoint_path}')
-                checkpoint_path = None
-
-        # Load the experiment configuration.
-        arguments_path = os.path.join(path, 'config.yaml')
-        with open(arguments_path, 'r') as config_file:
-            config = yaml.load(config_file, Loader=yaml.FullLoader)
-        config = argparse.Namespace(**config)
-
-        header = header or config.header
-        agent = agent or config.agent
-        environment = environment or config.test_environment
-        environment = environment or config.environment
-        trainer = trainer or config.trainer
-
-    # Run the header first, e.g. to load an ML framework.
-    if header:
-        exec(header)
-
-    # Build the training environment.
-    _environment = environment
-    environment = tonic.environments.distribute(
-        lambda: eval(_environment), parallel, sequential)
-    environment.initialize(seed=seed)
-
-    # Build the testing environment.
-    _test_environment = test_environment if test_environment else _environment
-    test_environment = tonic.environments.distribute(
-        lambda: eval(_test_environment))
-    test_environment.initialize(seed=seed + 10000)
-
-    # Build the agent.
-    if not agent:
-        raise ValueError('No agent specified.')
-    agent = eval(agent)
-    agent.initialize(
-        observation_space=environment.observation_space,
-        action_space=environment.action_space, seed=seed)
-
-    # Load the weights of the agent form a checkpoint.
-    if checkpoint_path:
-        agent.load(checkpoint_path)
-
-    # Initialize the logger to save data to the path environment/name/seed.
-    if not environment_name:
-        if hasattr(test_environment, 'name'):
-            environment_name = test_environment.name
-        else:
-            environment_name = test_environment.__class__.__name__
-    if not name:
-        if hasattr(agent, 'name'):
-            name = agent.name
-        else:
-            name = agent.__class__.__name__
-        if parallel != 1 or sequential != 1:
-            name += f'-{parallel}x{sequential}'
-    path = os.path.join(environment_name, name, str(seed))
-    tonic.logger.initialize(path, script_path=__file__, config=args)
-
-    # Build the trainer.
-    trainer = trainer or 'tonic.Trainer()'
-    trainer = eval(trainer)
-    trainer.initialize(
-        agent=agent, environment=environment,
-        test_environment=test_environment)
-
-    # Run some code before training.
-    if before_training:
-        exec(before_training)
-
-    # Train.
-    trainer.run()
-
-    # Run some code after training.
-    if after_training:
-        exec(after_training)
+from tonic import logger
 
 
-if __name__ == '__main__':
-    # Argument parsing.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--header')
-    parser.add_argument('--agent')
-    parser.add_argument('--environment', '--env')
-    parser.add_argument('--test_environment', '--test_env')
-    parser.add_argument('--trainer')
-    parser.add_argument('--before_training')
-    parser.add_argument('--after_training')
-    parser.add_argument('--parallel', type=int, default=1)
-    parser.add_argument('--sequential', type=int, default=1)
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--name')
-    parser.add_argument('--environment_name')
-    parser.add_argument('--checkpoint', default='last')
-    parser.add_argument('--path')
+class Trainer:
+    '''Trainer used to train and evaluate an agent on an environment.'''
 
-    args = vars(parser.parse_args())
-    train(**args)
+    def __init__(
+        self, steps=int(1e7), epoch_steps=int(2e4), save_steps=int(5e5),
+        test_episodes=5, show_progress=True, replace_checkpoint=False,
+    ):
+        self.max_steps = steps
+        self.epoch_steps = epoch_steps
+        self.save_steps = save_steps
+        self.test_episodes = test_episodes
+        self.show_progress = show_progress
+        self.replace_checkpoint = replace_checkpoint
+
+    def initialize(self, agent, environment, test_environment=None):
+        self.agent = agent
+        self.environment = environment
+        self.test_environment = test_environment
+
+    def run(self):
+        '''Runs the main training loop.'''
+
+        start_time = last_epoch_time = time.time()
+
+        # Start the environments.
+        observations = self.environment.start()
+
+        num_workers = len(observations)
+        scores = np.zeros(num_workers)
+        lengths = np.zeros(num_workers, int)
+        self.steps, epoch_steps, epochs, episodes = 0, 0, 0, 0
+        steps_since_save = 0
+
+        while True:
+            # Select actions.
+            actions = self.agent.step(observations, self.steps)
+            assert not np.isnan(actions.sum())
+            logger.store('train/action', actions, stats=True)
+
+            # Take a step in the environments.
+            observations, infos = self.environment.step(actions)
+            self.agent.update(**infos, steps=self.steps)
+
+            scores += infos['rewards']
+            lengths += 1
+            self.steps += num_workers
+            epoch_steps += num_workers
+            steps_since_save += num_workers
+
+            # Show the progress bar.
+            if self.show_progress:
+                logger.show_progress(
+                    self.steps, self.epoch_steps, self.max_steps)
+
+            # Check the finished episodes.
+            for i in range(num_workers):
+                if infos['resets'][i]:
+                    logger.store('train/episode_score', scores[i], stats=True)
+                    logger.store(
+                        'train/episode_length', lengths[i], stats=True)
+                    scores[i] = 0
+                    lengths[i] = 0
+                    episodes += 1
+
+            # End of the epoch.
+            if epoch_steps >= self.epoch_steps:
+                # Evaluate the agent on the test environment.
+                if self.test_environment:
+                    self._test()
+
+                # Log the data.
+                epochs += 1
+                current_time = time.time()
+                epoch_time = current_time - last_epoch_time
+                sps = epoch_steps / epoch_time
+                logger.store('train/episodes', episodes)
+                logger.store('train/epochs', epochs)
+                logger.store('train/seconds', current_time - start_time)
+                logger.store('train/epoch_seconds', epoch_time)
+                logger.store('train/epoch_steps', epoch_steps)
+                logger.store('train/steps', self.steps)
+                logger.store('train/worker_steps', self.steps // num_workers)
+                logger.store('train/steps_per_second', sps)
+                logger.dump()
+                last_epoch_time = time.time()
+                epoch_steps = 0
+
+            # End of training.
+            stop_training = self.steps >= self.max_steps
+
+            # Save a checkpoint.
+            if stop_training or steps_since_save >= self.save_steps:
+                path = os.path.join(logger.get_path(), 'checkpoints')
+                if os.path.isdir(path) and self.replace_checkpoint:
+                    for file in os.listdir(path):
+                        if file.startswith('step_'):
+                            os.remove(os.path.join(path, file))
+                checkpoint_name = f'step_{self.steps}'
+                save_path = os.path.join(path, checkpoint_name)
+                # --- APPLY THE CHANGE HERE ---
+                save_path = f"{save_path}.weights.h5" # Append the required extension
+                # --- END CHANGE ---
+                self.agent.save(save_path)
+                steps_since_save = self.steps % self.save_steps
+
+            if stop_training:
+                break
+
+    def _test(self):
+        '''Tests the agent on the test environment.'''
+
+        # Start the environment.
+        if not hasattr(self, 'test_observations'):
+            self.test_observations = self.test_environment.start()
+            assert len(self.test_observations) == 1
+
+        # Test loop.
+        for _ in range(self.test_episodes):
+            score, length = 0, 0
+
+            while True:
+                # Select an action.
+                actions = self.agent.test_step(
+                    self.test_observations, self.steps)
+                assert not np.isnan(actions.sum())
+                logger.store('test/action', actions, stats=True)
+
+                # Take a step in the environment.
+                self.test_observations, infos = self.test_environment.step(
+                    actions)
+                self.agent.test_update(**infos, steps=self.steps)
+
+                score += infos['rewards'][0]
+                length += 1
+
+                if infos['resets'][0]:
+                    break
+
+            # Log the data.
+            logger.store('test/episode_score', score, stats=True)
+            logger.store('test/episode_length', length, stats=True)
